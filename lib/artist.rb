e95180107b629e7de744c231bce28ff2405e7d7d
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-03-11 21:49:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,14 +20,6 @@ class Artist
     @@artists
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -37,7 +29,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
