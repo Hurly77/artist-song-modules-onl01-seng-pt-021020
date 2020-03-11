8b38de43df40a326678b80d82f552beb1cfbbd47
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-03-11 20:25:32 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,13 +1,11 @@
 module Memorable
   extend Memorable::Artist
   extend Song::ClassMethods
-  module ClassMethods
   def self.reset_all
     self.all.clear
   end
 
   def self.count
     self.all.count
-  end
 end
 end
